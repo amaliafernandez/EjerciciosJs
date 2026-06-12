64d03e435fc06f21db72be5dfa7b9bc60221b378
amalia fernandez <m.amalia.fernandez@gmail.com>
2026-06-12 11:38:46 -0300

    Resolucion de ejercicio: a partir del ingreso de cierta cantidad de horas puedas obtener la cantidad de minutos

---
+++ js/ejercicioCalculadorMinutos.js
@@ -0,0 +1,9 @@
+//El calculador de minutos:  Diseñar un algoritmo que permita al usuario ingresar una cantidad de horas
+// y muestre por pantalla a cuántos minutos equivalen.
+
+let numero , resultado
+numero = parseFloat(prompt("ingrese un numero"))
+console.log(numero)
+
+resultado = numero * 60
+console.log("la cantidad de minutos es de: " + resultado)
